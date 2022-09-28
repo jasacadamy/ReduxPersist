be1e911939a3b2f692c25881fa7c4f4dcdab8edf
mac <mac@macs-MacBook-Pro.local>
2022-09-28 13:36:15 +0530

    formik

--- src/Auth/LoginScreen.js
+++ src/Auth/LoginScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import {authenticate} from '../Reducer/AuthReducer';
 import {useDispatch} from 'react-redux';
+
+import {Formik} from 'formik';
 
 const LoginScreen = () => {
   const dispatch = useDispatch();
@@ -15,17 +17,57 @@ const LoginScreen = () => {
     dispatch(authenticate());
     navigation.navigate('Home');
   };
+  const handleSubmit = () => {};
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text style={{fontSize: 30, color: 'black', marginBottom: 30}}>
         Login Screen
       </Text>
-      <View style={{flexDirection: 'row'}}>
-        <Button onPress={onLoginPress} title="Login" color="#841584" />
-        <View style={{width: 40}} />
-        <Button onPress={onRegisterPress} title="Register" color="#841584" />
-      </View>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <>
+            <View>
+              <TextInput
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                style={style.inputContainer}
+                placeholder="Enter email"
+              />
+              <View style={{height: 20}} />
+              <TextInput
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                style={style.inputContainer}
+                placeholder="Enter password"
+                secureTextEntry
+              />
+            </View>
+            <View style={{flexDirection: 'row', marginTop: 50}}>
+              <Button onPress={handleSubmit} title="Login" color="#841584" />
+              <View style={{width: 40}} />
+              <Button
+                onPress={onRegisterPress}
+                title="Register"
+                color="#841584"
+              />
+            </View>
+          </>
+        )}
+      </Formik>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  inputContainer: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 250,
+    padding: 10,
+  },
+});
 export default LoginScreen;
--- src/Auth/RegisterScreen.js
+++ src/Auth/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {Formik} from 'formik';
 
 const RegisterScreen = () => {
   const navigation = useNavigation();
@@ -12,8 +13,49 @@ const RegisterScreen = () => {
       <Text style={{fontSize: 30, color: 'black', marginBottom: 30}}>
         Register Screen
       </Text>
-      <Button onPress={onSubmitPress} title="Register" color="#841584" />
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <>
+            <View>
+              <TextInput
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                style={style.inputContainer}
+                placeholder="Enter email"
+              />
+              <View style={{height: 20}} />
+              <TextInput
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                style={style.inputContainer}
+                placeholder="Enter password"
+                secureTextEntry
+              />
+            </View>
+            <View style={{flexDirection: 'row', marginTop: 50}}>
+              <Button
+                onPress={onSubmitPress}
+                title="Register"
+                color="#841584"
+              />
+            </View>
+          </>
+        )}
+      </Formik>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  inputContainer: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 250,
+    padding: 10,
+  },
+});
 export default RegisterScreen;
